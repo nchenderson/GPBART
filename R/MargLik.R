fe8c00c203206d1a08c1780aeae6a3728c4950d4
Nicholas Henderson <nchender@NicholassiMac2.attlocal.net>
2021-02-20 15:43:10 -0500

    changed parameterization of hyperparameters

--- R/MargLik.R
+++ R/MargLik.R
@@ -1,12 +1,15 @@
-MargLogLik <- function(hyper.pars, y, X, nu=3, H = 200, weights=NULL) {
+MargLogLik <- function(hyper.pars, y, X, sigest, nu=3, H = 200, weights=NULL) {
+    ## The input hyperparameters are (alpha, beta, kappa, q)
     alpha <- hyper.pars[1]
     bbeta <- hyper.pars[2]
     kappa <- hyper.pars[3]
-    lambda <- hyper.pars[4]
-    
+    qq <- hyper.pars[4]
+    lambda <- ((sigest*sigest)/nu)*qchisq(1 - qq, df=nu)
+    print(hyper.pars)
     # Compute Covariance matrix bSigma here
     # This will use values of (alpha, beta, sigma.mu)
     hp <- c(alpha, bbeta, 1/(4*kappa*kappa*H), lambda)
+    n <- length(y)
     tmp <- FindCovMat(X, hyper.pars=hp, weights=weights)
     bSigma <- H*tmp
     SS <- bSigma
@@ -14,15 +17,15 @@ MargLogLik <- function(hyper.pars, y, X, nu=3, H = 200, weights=NULL) {
         ret <- rep(NA, length(u))
         for(k in seq_len(length(u))) {
            diag(SS) <- diag(bSigma) + u[k]   
-           ldet <- determinant(SS, logarithm=TRUE)$modulus
+           ldet <- determinant(SS, logarithm=TRUE)$modulus + n*log(2*pi)
            ssterm <- as.numeric(crossprod(y, solve(SS, y)))
-           lsig.prior <- (nu + 2)*log(u[k]) + (nu*lambda)/u[k] - nu*log(lambda)
+           lsig.prior <- (nu + 2)*log(u[k]) + (nu*lambda)/u[k]
            ret[k] <- exp(-ldet/2 - ssterm/2 - lsig.prior/2)
         }
         return(ret)
     }
     ans <- integrate(integrand.fn, lower=0, upper=Inf)$value
-    return(-log(ans)) # return negative log-likelihood
+    return(-log(ans) - (nu/2)*log(lambda)) # return negative log-likelihood
 }
 
 #FindHypers <- function(y, X) {
